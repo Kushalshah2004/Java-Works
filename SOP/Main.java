00fe9185e4ed3d1d766553938f7745b4a6d8baf6
Kushal Shah <111559757+Kushalshah2004@users.noreply.github.com>
2025-06-19 18:39:45 +0530

    Delete SOP directory

--- SOP/Main.java
+++
@@ -1,8 +0,0 @@
-import java.io.*;
-class Main{
-	public static void main(String args[]){
-		PrintStream Kushal = System.out;
-		Kushal.println("Hello World");
-		//System.out.println("Hello World");
-	}
-}

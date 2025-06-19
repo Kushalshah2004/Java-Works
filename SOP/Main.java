e27ccaae188acc60b68ecef311dd7fe22a6e14c8
Kushal Shah <111559757+Kushalshah2004@users.noreply.github.com>
2025-06-19 18:39:21 +0530

    Add files via upload

---
+++ SOP/Main.java
@@ -0,0 +1,8 @@
+import java.io.*;
+class Main{
+	public static void main(String args[]){
+		PrintStream Kushal = System.out;
+		Kushal.println("Hello World");
+		//System.out.println("Hello World");
+	}
+}
